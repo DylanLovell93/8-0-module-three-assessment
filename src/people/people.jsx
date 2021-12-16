a5d331e2ace9ff0a31f39bdd405001fffd6e298d
DylanLovell <arcanyne7@gmail.com>
2021-12-16 15:55:10 -0500

    state and event handlers for people

--- src/people/people.jsx
+++ src/people/people.jsx
@@ -1,13 +1,53 @@
 import { Component } from 'react';
 import './people.css';
+//import components
+import PersonSearch from './personSearch/personSearch';
+import PersonInfo from './personInfo/personInfo';
 
 class people extends Component {
   constructor() {
     super();
+    this.state = {
+      allPeople: '',
+      searchResult: '',
+      search: '',
+    };
   }
 
+  componentDidMount() {
+    fetch('https://ghibliapi.herokuapp.com/people')
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          allPeople: data,
+        })
+      );
+  }
+  handleInput = (event) => {
+    const currentInput = event.target.value;
+    this.setState({
+      search: currentInput,
+    });
+  };
+
+  handleSubmit = () => {
+    const { search, allPeople } = this.state;
+    const targetPerson = allPeople.find(
+      (person) => person.name.toLowerCase() === search
+    );
+    this.setState({
+      searchResult: targetPerson,
+    });
+  };
+
   render() {
-    return <div className="people">people</div>;
+    return (
+      <div className="people">
+        <h1>Select a Movie</h1>
+        <PersonSearch />
+        <PersonInfo />
+      </div>
+    );
   }
 }
 
