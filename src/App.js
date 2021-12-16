04fbb4b01107c7fb783a2f733bb4ab923a21ea4e
DylanLovell <arcanyne7@gmail.com>
2021-12-16 11:54:01 -0500

    light wireframing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import { Component } from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+//Import components
+import Nav from './nav/nav';
+import Home from './home/home';
+import Movies from './movies/movies';
+import Locations from './locations/locations';
+import People from './people/people';
 
 class App extends Component {
   constructor() {
@@ -7,7 +15,25 @@ class App extends Component {
   }
 
   render() {
-    return <div className="App">App</div>;
+    return (
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/movies">
+            <Movies />
+          </Route>
+          <Route path="/locations">
+            <Locations />
+          </Route>
+          <Route path="/people">
+            <People />
+          </Route>
+        </Switch>
+      </div>
+    );
   }
 }
 
