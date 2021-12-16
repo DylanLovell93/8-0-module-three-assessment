55bd9364b1ceb6436258f48671033ee121bcda23
DylanLovell <arcanyne7@gmail.com>
2021-12-16 10:30:49 -0500

    templates for wireframing

---
+++ .templates/templateClass.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './template.css';
+
+class template extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div className="template"></div>;
+  }
+}
+
+export default template;
