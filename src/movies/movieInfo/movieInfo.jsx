5471a160e81c460e11eadd5f30f0bf88dae66885
DylanLovell <arcanyne7@gmail.com>
2021-12-16 14:49:36 -0500

    movie info, movie v1

--- src/movies/movieInfo/movieInfo.jsx
+++ src/movies/movieInfo/movieInfo.jsx
@@ -1,7 +1,15 @@
 import './movieInfo.css';
 
 const movieInfo = (props) => {
-  return <div className="movieInfo">movieInfo</div>;
+  const { movie } = props;
+  const info = (
+    <>
+      <h2>{movie.title}</h2>
+      <p>{movie.release_date}</p>
+      <p>{movie.description}</p>
+    </>
+  );
+  return <div className="movieInfo">{movie && info}</div>;
 };
 
 export default movieInfo;
