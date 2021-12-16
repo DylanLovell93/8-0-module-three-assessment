4036c46da9786dcc16429b2cf1ceffa1ef2b29b5
DylanLovell <arcanyne7@gmail.com>
2021-12-16 13:05:50 -0500

    nav links done

--- src/nav/navLinks/navLinks.jsx
+++ src/nav/navLinks/navLinks.jsx
@@ -1,7 +1,22 @@
 import './navLinks.css';
+import { Link } from 'react-router-dom';
 
 const navLinks = () => {
-  return <div className="navLinks">navLinks</div>;
+  return (
+    <div className="navLinks">
+      <ul>
+        <li>
+          <Link to="/movies">Movies</Link>
+        </li>
+        <li>
+          <Link to="/people">People</Link>
+        </li>
+        <li>
+          <Link to="/location">Location</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default navLinks;
